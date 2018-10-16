f717e8e98e27b87170c706f9dbebc2c2914e7186
PC1604967.ad.pisd.edu <PC1604967.ad.pisd.edu>
2018-10-16 11:42:37 -0500

    swzag

---
+++ Unit 05 - Set Lab/Tester.java
@@ -0,0 +1,23 @@
+
+/**
+ * 	Tester for the WordList Class
+ *  
+ *  The imports were placed above this heading because the class' heading 
+ *  should be the first thing above the class
+ * @author zayd.moosajee.1
+ * @teacher Ishman
+ * @dateDue Thursday, 10/16/18
+ * @collabWith me, myself, and I
+ */
+public class Tester 
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+}
---
+++ Unit 05 - Set Lab/WordList.java
@@ -0,0 +1,58 @@
+import java.util.Set;
+
+/**
+ * 	Creates an alphabetized collection of: the unique words in a given text file,
+ *  words common to two sets of words from different given files, and all words 
+ *  used in both of these files.
+ *  
+ *  The imports were placed above this heading because the class' heading 
+ *  should be the first thing above the class
+ * @author zayd.moosajee.1
+ * @teacher Ishman
+ * @dateDue Thursday, 10/16/18
+ * @collabWith me, myself, and I
+ */
+public class WordList
+{
+	
+	/**
+	 * 
+	 * @param file
+	 */
+	public WordList(String file)
+	{
+	}
+	
+	/**
+	 * @param file
+	 */
+	public void setFile(String file)
+	{
+	}
+	
+	/**
+	 * @return
+	 */
+	public Set<String> getWords()
+	{
+		return null;
+	}
+	
+	/**
+	 * @param moreWords
+	 * @return
+	 */
+	public Set<String> getAllWords(Set<String> moreWords)
+	{
+		return null;
+	}
+	
+	/**
+	 * @param moreWords
+	 * @return
+	 */
+	public Set<String> getCommonWords(Set<String> moreWords)
+	{
+		return null;
+	}
+}
