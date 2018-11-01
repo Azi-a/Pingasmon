9df8f3d983ed1df0f5fc103975211405ed95b7ff
PC1604967.ad.pisd.edu <PC1604967.ad.pisd.edu>
2018-11-01 11:46:34 -0500

    work on IO class-getistke

--- src/PmonIO.java
+++ src/PmonIO.java
@@ -1,33 +1,59 @@
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 /**
  * @author zayd.moosajee.1
  *
- * @param <T> type of the object being loaded/saved
+ * @param <T> the type of the object being loaded/saved
  */
 public class PmonIO<T> implements Serializable
 {
-
-
-	private PrintWriter out;
-	private Scanner in;
+	//for specified update checks on the first few lines of an object
+	private static PrintWriter out;
+	private static Scanner in;
 	
-
+	//loader
+	private static FileInputStream fileIn;
+	private static ObjectInputStream objIn;
 	
-	FileInputStream fileIn;
-	ObjectInputStream objIn;
+	//saver
+	private static FileOutputStream fileOut;
+	private ObjectOutputStream objOut;
+	
+	//target File
+	private static File target;
+	
+	private HashMap tracer;
 	
 	
 	/**
 	 * 
 	 */
-	public void save()
+	public PmonIO() 
 	{
+		super();
 		
+	}
+
+	
+	
+	/**
+	 * saves the given object T
+	 */
+	public void save(T saveMe)
+	{
+		fileOut = new FileOutputStream("null(?)");
+		objOut = new ObjectOutputStream(fileOut);
+		objOut.writeObject(saveMe);
 	}
 	
 	/**
@@ -39,6 +65,6 @@ public class PmonIO<T> implements Serializable
 		
 	}
 	
-	
+	//make a hashtree to point file/object/trainer names to their locations on disk.
 	
 }
